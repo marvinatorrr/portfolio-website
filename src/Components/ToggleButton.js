4cc2a745e2155d8f48254fe37e2bba9205778c54
marvinatorrr <marvinatorrr@gmail.com>
2022-03-25 19:23:36 +0800

    added bg color transition when hovering over button

--- src/Components/ToggleButton.js
+++ src/Components/ToggleButton.js
@@ -2,7 +2,10 @@ import { CgSun } from "react-icons/cg";
 
 function ToggleButton() {
   return (
-    <button className="bg-[#fbd38d] w-[40px] h-[40px] rounded-lg flex justify-center items-center ">
+    <button
+      className="bg-[#fbd38d] w-[40px] h-[40px] rounded-lg flex justify-center items-center
+     hover:bg-yellow-500 transition-colors duration-500 ease-out"
+    >
       <CgSun color="black" />
     </button>
   );
