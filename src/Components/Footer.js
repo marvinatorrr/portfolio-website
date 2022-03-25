5335158add6e38e13f4db493eba941d86153c734
marvinatorrr <marvinatorrr@gmail.com>
2022-03-25 19:26:20 +0800

    amend footer font

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center text-base text-gray-500">
       <p>© 2022 Marvin Ho Zhi Quan. All Rights Reserved.</p>
     </div>
   );
