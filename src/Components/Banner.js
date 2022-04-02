e7eb89f81d0cb8e414d4c6580653b358fb3a45c7
marvinatorrr <marvinatorrr@gmail.com>
2022-04-02 20:47:58 +0800

    banner border

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -10,10 +10,9 @@ function Model() {
 function Banner() {
   return (
     <div className="flex justify-center">
-      <div className="w-[485.5px] h-[400px]">
-        <Canvas className="" shadowMap camera={{ position: [0, 3, 0] }}>
+      <div className="w-[485.5px] h-[250px] border-solid border-2 border-sky-500 rounded-lg">
+        <Canvas camera={{ position: [0, 1, 0] }}>
           <ambientLight intensity={0.4} />
-
           <Suspense fallback={null}>
             <Model />
           </Suspense>
