2332c89c5138e19741ca72e36af099eb49f119b6
marvinatorrr <marvinatorrr@gmail.com>
2022-04-02 19:22:07 +0800

    render model

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,36 +1,33 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import React, { Suspense, useState, useEffect } from "react";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+function Model() {
+  const gltf = useGLTF("/modelDraco.gltf");
+  return <primitive object={gltf.scene} />;
+}
 
 function Banner() {
   return (
     <div className="flex justify-center">
-      <Canvas shadowMap camera={{ position: [0, 0, 15] }}>
-        <ambientLight intensity={0.75} />
-        <pointLight intensity={1} position={[-10, -25, -10]} />
-        <spotLight
-          castShadow
-          intensity={2.25}
-          angle={0.2}
-          penumbra={1}
-          position={[25, 25, 25]}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-bias={-0.0001}
-        />
-        <fog attach="fog" args={["#cc7b32", 16, 20]} />
-        <Suspense fallback={null}></Suspense>
-        <OrbitControls
-          autoRotate
-          enablePan={false}
-          enableZoom={false}
-          enableDamping
-          dampingFactor={0.5}
-          rotateSpeed={1}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-      </Canvas>
+      <div className="w-[485.5px] h-[400px]">
+        <Canvas className="" shadowMap camera={{ position: [0, 3, 0] }}>
+          <ambientLight intensity={0.4} />
+
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+          <OrbitControls
+            autoRotate
+            enablePan={false}
+            enableZoom={true}
+            enableDamping
+            dampingFactor={0.5}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 3}
+          />
+        </Canvas>
+      </div>
     </div>
   );
 }
