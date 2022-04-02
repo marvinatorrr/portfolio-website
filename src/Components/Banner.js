65ded2d41f25eae4931445745a1472246dc69296
Zoe Chen <ZOEYCHEN@MacBook-Air.local>
2022-04-02 17:18:14 +0800

    hi

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,11 +1,36 @@
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useState, useEffect } from "react";
+import { OrbitControls } from "@react-three/drei";
+
 function Banner() {
   return (
     <div className="flex justify-center">
-      <img
-        src="../Resources/Banner.jpg"
-        alt="My banner"
-        className="w-[485.5px]"
-      ></img>
+      <Canvas shadowMap camera={{ position: [0, 0, 15] }}>
+        <ambientLight intensity={0.75} />
+        <pointLight intensity={1} position={[-10, -25, -10]} />
+        <spotLight
+          castShadow
+          intensity={2.25}
+          angle={0.2}
+          penumbra={1}
+          position={[25, 25, 25]}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-bias={-0.0001}
+        />
+        <fog attach="fog" args={["#cc7b32", 16, 20]} />
+        <Suspense fallback={null}></Suspense>
+        <OrbitControls
+          autoRotate
+          enablePan={false}
+          enableZoom={false}
+          enableDamping
+          dampingFactor={0.5}
+          rotateSpeed={1}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+      </Canvas>
     </div>
   );
 }
