46b536ebc38ce40bfa7fbb70e9a9f8a1274f51d8
marvinatorrr <marvinatorrr@gmail.com>
2022-04-20 21:19:54 +0800

    home page mobile optimization

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,33 +5,33 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="h-14 flex justify-center fixed top-0 bg-opacity-75 backdrop-blur-sm	 bg-[#f5f0e8] dark:bg-[#202023] text-black dark:text-white text-base dark:bg-opacity-75 dark:backdrop-blur-sm z-50">
-      <div className="flex w-screen justify-between">
-        <div className="flex ml-2">
+    <div className="h-14 w-full flex justify-center fixed top-0 bg-opacity-75 backdrop-blur-sm	 bg-[#f5f0e8] dark:bg-[#202023] text-black dark:text-white text-base dark:bg-opacity-75 dark:backdrop-blur-sm z-50">
+      <div className="flex w-[375px] sm:w-[485.5px] justify-between">
+        <div className="flex">
           <div className="flex items-center text-xl font-extrabold">
             <Link to="/" className="flex">
               <GiKingJuMask size={30} className="mr-1" />
               Marvin Ho
             </Link>
           </div>
-          {/* <Link
+          <Link
             to="works"
-            className="mx-3 hover:underline underline-offset-2 flex items-center"
+            className="mx-4 hover:underline underline-offset-2 flex items-center"
           >
             Works
-          </Link> */}
+          </Link>
 
-          {/* <a
+          <a
             href="https://github.com/marvinatorrr/portfolio-website"
-            className="mx-3 hover:underline underline-offset-2 flex items-center"
+            className="mx-2 hover:underline underline-offset-2 flex items-center"
             target="_blank"
             rel="noopener noreferrer"
           >
             {<BsGithub className="mr-2" />}Source
-          </a> */}
+          </a>
         </div>
 
-        <div className="flex items-center mr-2">
+        <div className="flex items-center">
           <ToggleButton></ToggleButton>
         </div>
       </div>
